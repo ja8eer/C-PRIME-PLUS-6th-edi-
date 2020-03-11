e93fe337b6c3d816cf8766ae1e9ef87674afbf8f
ja8eer <57190486+ja8eer@users.noreply.github.com>
2020-03-11 17:58:28 +0530

    Add files via upload

---
+++ chp1/prg_exercise3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void first();
+void second();
+int main(){
+	cout<<"Three blind mice\n";
+	first();
+	second();
+	cout<<"See how they run\n";
+}
+void first(){
+	cout<<"Three bling mice\n";
+}
+void second(){
+	cout<<"See how they run\n";
+}
