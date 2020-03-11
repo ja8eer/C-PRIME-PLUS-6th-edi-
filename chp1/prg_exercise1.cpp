39a9007d546976a9afe8f803c70da6bc5b84f2e6
ja8eer <57190486+ja8eer@users.noreply.github.com>
2020-03-11 17:20:48 +0530

    Add files via upload

---
+++ chp1/prg_exercise1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"enter you name :";
+	char name[20];
+	cin>>name;
+	cout<<"enter your age :";
+	int age;
+	cin>>age;
+	cout<<"enter your house name :";
+	char house[30];
+	cin>>house;
+	cout<<"your name is "<<name<<endl;
+	cout<<"your age is "<<age<<endl;
+	cout<<"your house name is "<<house<<endl;
+
+
+}
