336434e1632caec9c306aae224827d23708f7826
ja8eer <57190486+ja8eer@users.noreply.github.com>
2020-03-11 17:49:42 +0530

    Add files via upload

---
+++ chp1/prg_exercise2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int furlong;
+	int yard=220;
+	cout<<"enter the number in the furlong :";
+	cin>>furlong;
+	cout<<"after the converting \n";
+	yard *= furlong;
+	cout<<"for "<<furlong<<" the yard is "<<yard<<endl;
+}
+
